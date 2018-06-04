d36496c266de6e3e9e362fc9353a8711e806e49c
dvansa <daniel.vansa@gmail.com>
2018-06-05 01:15:25 +0200

    Added submodules for dependencies. CMake to build imgui static lib. Script to one-click build with VC14 2015

---
+++ dependencies/build_vc14.bat
@@ -0,0 +1,30 @@
+@echo off
+net session>nul 2>&1
+if %ERRORLEVEL% equ 0 (
+    echo Administrator OK
+) ELSE (
+    
+    goto :ERROREND
+)
+
+@echo on
+
+:: Create build dir
+mkdir build 2>NUL
+cd build
+
+:: VS2014 env
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+
+:: Build
+cmake -G "Visual Studio 14 2015" ..
+msbuild /p:configuration=Debug /p:platform=Win32 Dependencies.sln
+msbuild /p:configuration=Debug /p:platform=Win32 INSTALL.vcxproj
+
+goto :END
+
+:ERROREND
+echo Admin privileges are required
+
+:END
+
